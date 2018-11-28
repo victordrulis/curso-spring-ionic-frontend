07847aa120f27bec52172be4c3c268e27d0b316a
Victor Drulis <victordrulis@gmail.com>
2018-11-28 14:37:56 -0200

    Desabilitar menu na tela inicial

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -10,16 +10,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-categorias',
-  templateUrl: 'categorias.html',
+    selector: 'page-categorias',
+    templateUrl: 'categorias.html',
 })
 export class CategoriasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriasPage');
-  }
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad CategoriasPage');
+    }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, MenuController } from 'ionic-angular';
 import { IonicPage } from 'ionic-angular/navigation/ionic-page';
 
 @IonicPage()
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, public menu: MenuController) {
 
-  }
+    }
 
-  login() {
-    this.navCtrl.setRoot('CategoriasPage');
-  }
+    ionViewWillEnter() {
+        this.menu.swipeEnable(false);
+    }
+
+    ionViewDidLeave() {
+        this.menu.swipeEnable(true);
+    }
+
+    login() {
+        this.navCtrl.setRoot('CategoriasPage');
+    }
 
 }
