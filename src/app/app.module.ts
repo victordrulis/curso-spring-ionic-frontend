7ac00241e7879e2dfa63f650402b4a38a3bcc089
Victor Drulis <victordrulis@gmail.com>
2018-12-03 23:03:31 -0200

    Salvando os dados do usuario no LocalStorage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { StorageService } from './../services/storage.service';
 import { AuthService } from './../services/auth.service';
 import { CategoriaService } from './../services/domain/categoria.service';
 import { BrowserModule } from '@angular/platform-browser';
@@ -12,25 +13,26 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { ErrorInterceptorProvider } from '../interceptor/error-interceptor';
 
 @NgModule({
-  declarations: [
-    MyApp
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    IonicModule.forRoot(MyApp),
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    CategoriaService,
-    ErrorInterceptorProvider,
-    AuthService
-  ]
+    declarations: [
+        MyApp
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        IonicModule.forRoot(MyApp),
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        { provide: ErrorHandler, useClass: IonicErrorHandler },
+        CategoriaService,
+        ErrorInterceptorProvider,
+        AuthService,
+        StorageService
+    ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,3 +1,5 @@
+import { StorageService } from './storage.service';
+import { LocalUser } from './../models/local_user';
 import { API_CONFIG } from './../config/api.config';
 import { HttpClient } from '@angular/common/http';
 import { CredencaisDTO } from './../models/credenciais.dto';
@@ -6,7 +8,7 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class AuthService {
     
-    constructor(public http : HttpClient) {
+    constructor(public http : HttpClient, public storage : StorageService) {
 
     }
 
@@ -14,5 +16,17 @@ export class AuthService {
         return this.http.post(`${API_CONFIG.baseUrl}/login`, creds,
             {observe: 'response', responseType: 'text'}
         );
+    }
+
+    successfulLogin(authorizationValue : string) {
+        let tok = authorizationValue.substring(7);
+        let usr : LocalUser = {
+            token: tok
+        };
+        this.storage.setLocalUser(usr);
+    }
+
+    logout() {
+        this.storage.setLocalUser(null);
     }
 }
